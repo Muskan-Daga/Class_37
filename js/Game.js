fa2d498a32bf18ddd9bac3695a9ad200c731f38a
Muskan-Daga <78206639+Muskan-Daga@users.noreply.github.com>
2021-07-02 17:20:24 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,53 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player=new Player();
+      var playerCountref = await database.ref("playerCount").once("value");
+      if(playerCountref.exists()){
+        playerCount=playerCountref.val();
+        player.getCount();
+      }
+        form = new Form()
+        form.display();
+      }
+    }
+
+    play(){
+      form.hide();
+      text("GAME STARTS",120,100);
+      Player.playerInfo();
+
+      if(allPlayers !== undefined){
+        var dp=130;
+        for(var plr in allPlayers){
+          if(plr == 'player'+ player.index)
+          fill("red")
+          else
+          fill("black")
+          dp+=20
+          textSize(20) 
+          text(allPlayers[plr].name+":"+allPlayers[plr].distance,120,dp)
+        }
+      }
+      if(keyIsDown(UP_ARROW) && player.index !== null){
+        player.distance+=50;
+        player.update();
+      }
+    }
+  }
